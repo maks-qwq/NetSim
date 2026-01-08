38154335eb50ee929223b9046d05561457fba8e7
maks <maks-zych@wp.pl>
2026-01-08 22:57:28 +0100

    Plik nagłówkowy netsim

---
+++ include/netsim.hpp
@@ -0,0 +1,14 @@
+#ifndef NETSIM_H // include guard
+#define NETSIM_H
+
+class Product {
+public:
+    Product() {} //tutaj musi być przydzielanie ID
+
+    
+
+private:
+    int id_;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "netsim.h"
+#include <iostream>
+
+int main(){
+
+    std::cout << "wykonuję się";
+
+    return 0;
+}
