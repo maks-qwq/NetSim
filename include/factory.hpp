ca4405a6518ce40411f8baf9e2c7194e16a531e5
Wiek86 <adrian.witek12@gmail.com>
2026-01-28 16:23:52 +0100

    Factory.hpp
    zmiany w factory.hpp

--- include/factory.hpp
+++ include/factory.hpp
@@ -2,13 +2,17 @@
 
 #include <map>
 #include <memory>
+#include <algorithm>
 
 #include "nodes.hpp"
 #include "storage_types.hpp"
-#include <bits/stdc++.h>
 
 enum class NodeColor { UNVISITED, VISITED, VERIFIED };
 bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors);
+
+enum class ElementType {
+    RAMP, WORKER, STOREHOUSE
+};
 
 template <typename Node>
 class NodeCollection {
@@ -19,21 +23,28 @@ public:
 
     void add(Node&& node) { nodes_.emplace_back(std::move(node)); } //dodanie nowego połączenia
 
-    void remove_by_id(ElementID id); //usuwa z kontenera po id
-
     iterator begin() {return nodes_.begin();}
     iterator end() {return nodes_.end();}
     const_iterator begin() const {return nodes_.cbegin();}
     const_iterator end() const {return nodes_.cend();}
-    const_iterator begin() const {return nodes_.cbegin();}
-    const_iterator end() const {return nodes_.cend();}
+    const_iterator cbegin() const {return nodes_.cbegin();}
+    const_iterator cend() const {return nodes_.cend();}
 
-    iterator find_by_id(ElementID id); //szuka elementu po id
-    const_iterator find_by_id(ElementID id) const;
-
+    NodeCollection<Node>::iterator find_by_id(ElementID id) {
+        return std::find_if(begin(), end(), [id](const Node& pack) {
+            return pack.get_id() == id;
+        });
+    }
+    NodeCollection<Node>::const_iterator find_by_id(ElementID id) const {
+        return std::find_if(begin(), end(), [id](const Node& pack) {
+            return pack.get_id() == id;
+        });
+    }
+    void remove_by_id(ElementID id); //usuwa z kontenera po id
 private:
     container_t nodes_;
 };
+
 
 class Factory {
 public:
@@ -41,6 +52,29 @@ public:
     void do_deliveries(); //dokonywanie ewentualnych dostaw na rampy
     void do_package_passing(); //dokonywanie ewentualnego przekazywania półproduktów
     void do_work(); //dokonywanie ewentualnego przetwarzania półproduktów przez robotników
+
+    void add_ramp(Ramp&& r) { ramps_.add(std::move(r)); }
+    void remove_ramp(ElementID id) { ramps_.remove_by_id(id); }
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) { return ramps_.find_by_id(id); }
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const { return ramps_.find_by_id(id); }
+    NodeCollection<Ramp>::const_iterator ramp_cbegin() const { return ramps_.cbegin(); }
+    NodeCollection<Ramp>::const_iterator ramp_cend() const { return ramps_.cend(); }
+
+    void add_ramp(Worker&& r) { workers_.add(std::move(r)); }
+    void remove_ramp(ElementID id) { workers_.remove_by_id(id); }
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) { return workers_.find_by_id(id); }
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const { return workers_.find_by_id(id); }
+    NodeCollection<Worker>::const_iterator worker_cbegin() const { return workers_.cbegin(); }
+    NodeCollection<Worker>::const_iterator worker_cend() const { return workers_.cend(); }
+
+    void add_ramp(Storehouse&& r) { storehouses_.add(std::move(r)); }
+    void remove_ramp(ElementID id) { storehouses_.remove_by_id(id); }
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) { return storehouses_.find_by_id(id); }
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const { return storehouses_.find_by_id(id); }
+    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const { return storehouses_.cbegin(); }
+    NodeCollection<Storehouse>::const_iterator storehouse_cend() const { return storehouses_.cend(); }
+
+
 
     NodeCollection<Ramp>& ramps();
     NodeCollection<Worker>& workers();
@@ -54,6 +88,7 @@ private:
     NodeCollection<Ramp> ramps_;
     NodeCollection<Worker> workers_;
     NodeCollection<Storehouse> storehouses_;
+    template<class Node>
     void remove_receiver(NodeCollection<Node>& collection, ElementID id); //pomocnicza metoda do usuwania odbiorcy 
 };
 
--- src/factory.cpp
+++ src/factory.cpp
@@ -1,22 +1,14 @@
 #include "factory.hpp"
 
 template <typename Node>
-typename NodeCollection<Node>::iterator 
-NodeCollection<Node>::find_by_id(ElementID id){
-    return std::find_if(begin(), end(), [id](const auto& package) {
-        return package.get_id() == id;});
-}
-
-template <typename Node>
-typename NodeCollection<Node>::iterator 
-NodeCollection<Node>::find_by_id(ElementID id) const {
-    return std::find_if(begin(), end(), [id](const auto& package) {
-        return package.get_id() == id;});
-}
-
-void NodeCollection<Node>::remove_by_id(ElementID id){
+void NodeCollection<Node>::remove_by_id(ElementID id) {
     auto it = find_by_id(id);
     if (it != end()) {
         nodes_.erase(it);
     }
+}
+
+void Factory::do_deliveries(Time t) {
+    for (auto &ramp : ramps_)
+        ramp.deliver_goods(t);
 }
