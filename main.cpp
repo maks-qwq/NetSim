94166a6f5ed7f265bc3dace76907a9ab6fcdd1bc
maks <maks-zych@wp.pl>
2026-01-29 12:11:07 +0100

    main netsin

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "netsim.hpp"
 #include <iostream>
 #include "package.hpp"
-
+#include "package.hpp"
+#include "types.hpp"
+#include "storage_types.hpp"
 
 int main(){
-    std::cout << "wykonuję się";
+    std::cout << "wykonuję się, NETSIM";
 
     return 0;
 
